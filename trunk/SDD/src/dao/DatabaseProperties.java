1f579e4d404e70b8c8825cc4da41fe014ade3b48
alex.dusenbery <alex.dusenbery@e52d38a4-b9ad-11de-b017-a9a2c7051f87>
2011-04-18 20:39:29 +0000

    1. Modify TaxonBase to use protected members. 2. Add taxon values to TaxonRank. 3. Add Dao to access Filename-to-taxon table.

---
+++ trunk/SDD/src/dao/DatabaseProperties.java
@@ -0,0 +1,31 @@
+package dao;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class DatabaseProperties extends Properties {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1548553521773765476L;
+
+	public DatabaseProperties() {
+		super();
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream("src/dao/database.properties");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			this.load(fis);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
