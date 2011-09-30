3352492498e165a7a6cc12f8668185c5d4cb852b
alex.dusenbery@gmail.com <alex.dusenbery@gmail.com@e52d38a4-b9ad-11de-b017-a9a2c7051f87>
2011-09-30 15:44:12 +0000

    1. Equals and hashCode methods for CharTreeCharacters/AbstractNodes. 2. Check for existence of a character tree node before adding to character tree.

--- trunk/SDD/src/sdd/CharTreeAbstractNode.java
+++ trunk/SDD/src/sdd/CharTreeAbstractNode.java
@@ -250,4 +250,35 @@ public class CharTreeAbstractNode {
         return this.any;
     }
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((parent == null) ? 0 : parent.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof CharTreeAbstractNode))
+			return false;
+		CharTreeAbstractNode other = (CharTreeAbstractNode) obj;
+		if (parent == null) {
+			if (other.parent != null)
+				return false;
+		} else if (!parent.equals(other.parent))
+			return false;
+		return true;
+	}
+
 }
--- trunk/SDD/src/sdd/CharTreeCharacter.java
+++ trunk/SDD/src/sdd/CharTreeCharacter.java
@@ -70,4 +70,36 @@ public class CharTreeCharacter
         this.character = value;
     }
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((character == null) ? 0 : character.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof CharTreeCharacter))
+			return false;
+		CharTreeCharacter other = (CharTreeCharacter) obj;
+		if (character == null) {
+			if (other.character != null)
+				return false;
+		} else if (!character.equals(other.character))
+			return false;
+		return true;
+	}
+
 }
