1e8bc360ab097b6e10fe0ccfb672fb64675480de
hong1.cui <hong1.cui@e52d38a4-b9ad-11de-b017-a9a2c7051f87>
2012-12-09 17:17:45 +0000

    classes generated by jaxb and they have problems/errors.

--- trunk/SDD/src/annotationSchema/jaxb/Key.java
+++ trunk/SDD/src/annotationSchema/jaxb/Key.java
@@ -27,7 +27,7 @@ import javax.xml.bind.annotation.XmlType;
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
- *         &lt;element ref="{}statement" maxOccurs="unbounded"/>
+ *         &lt;element ref="{}key_statement" maxOccurs="unbounded"/>
  *       &lt;/sequence>
  *     &lt;/restriction>
  *   &lt;/complexContent>
@@ -38,41 +38,41 @@ import javax.xml.bind.annotation.XmlType;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "", propOrder = {
-    "statement"
+    "keyStatement"
 })
-@XmlRootElement(name = "description")
-public class Description {
+@XmlRootElement(name = "key")
+public class Key {
 
-    @XmlElement(required = true)
-    protected List<Statement> statement;
+    @XmlElement(name = "key_statement", required = true)
+    protected List<KeyStatement> keyStatement;
 
     /**
-     * Gets the value of the statement property.
+     * Gets the value of the keyStatement property.
      * 
      * <p>
      * This accessor method returns a reference to the live list,
      * not a snapshot. Therefore any modification you make to the
      * returned list will be present inside the JAXB object.
-     * This is why there is not a <CODE>set</CODE> method for the statement property.
+     * This is why there is not a <CODE>set</CODE> method for the keyStatement property.
      * 
      * <p>
      * For example, to add a new item, do as follows:
      * <pre>
-     *    getStatement().add(newItem);
+     *    getKeyStatement().add(newItem);
      * </pre>
      * 
      * 
      * <p>
      * Objects of the following type(s) are allowed in the list
-     * {@link Statement }
+     * {@link KeyStatement }
      * 
      * 
      */
-    public List<Statement> getStatement() {
-        if (statement == null) {
-            statement = new ArrayList<Statement>();
+    public List<KeyStatement> getKeyStatement() {
+        if (keyStatement == null) {
+            keyStatement = new ArrayList<KeyStatement>();
         }
-        return this.statement;
+        return this.keyStatement;
     }
 
 }
